04fe2d78c72f7702133c8b45d80bb2563e5a583f
Oluwatobiloba <aoluwatobi6@gmail.com>
2023-07-10 01:47:24 -0700

    master

---
+++ index.js
@@ -0,0 +1,37 @@
+const table12 = "12";
+
+let row1 = " " +table12 * 1;
+tableRow1.innerHTML = row1;
+
+let row2 = " " +table12 * 2;
+tableRow2.innerHTML = row2;
+
+let row3 = " " +table12 * 3;
+tableRow3.innerHTML = row3;
+
+let row4 = " " +table12 * 4;
+tableRow4.innerHTML = row4;
+
+let row5 = " " +table12 * 5;
+tableRow5.innerHTML = row5;
+
+let row6 = " " +table12 * 6;
+tableRow6.innerHTML = row6;
+
+let row7 = " " +table12 * 7;
+tableRow7.innerHTML = row7;
+
+let row8 = " " +table12 * 8;
+tableRow8.innerHTML = row8;
+
+let row9 = " " +table12 * 9;
+tableRow9.innerHTML = row9;
+
+let row10 = " " +table12 * 10;
+tableRow10.innerHTML = row10;
+
+let row11 = " " +table12 * 11;
+tableRow11.innerHTML = row11;
+
+let row12 = " " +table12 * 12;
+tableRow12.innerHTML = row12;
